ba07a178f78d0cc294ee2f125dffb3f4b7edc5a2
yidaiweiren <2577110429@qq.com>
2019-04-27 18:00:54 +0800

    删除

--- venv/caishuzi-ver1.0.py
+++
@@ -1,9 +0,0 @@
-print("-----猜数字游戏-----")
-tmp=input("请输入你猜的数字:")
-#print(tmp)
-guess=int(tmp)
-if guess==8:
-    print("猜对了！")
-else:
-    print("猜错了！")
-print("-----游戏结束-----")
--- venv/conn.py
+++
@@ -1,8 +0,0 @@
-#连接数据库 并测试打印数据库版本
-import pymysql
-db=pymysql.connect("localhost","root","Cc151602","testdb")
-cursor=db.cursor()
-cursor.execute("SELECT VERSION()")
-data=cursor.fetchone()
-print ("Databases version:%s" % data)
-db.close()
